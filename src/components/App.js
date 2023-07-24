4a88692a4af36fff0c00b5d8133db9c97a6e9851
omarfarouq ahmed <omarfarouq.ahmed@student.moringaschool.com>
2023-07-25 00:11:43 +0300

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,28 @@
 // create your App component here
+import react, { useState, useEffect } from "react";
+
+const App = ()=>{
+    const [dogImage, setDogImage] = useState ('')
+    const [isLoaded, setIsLoaded] = useState(false);
+
+ useEffect(()=>{
+    fetch ('https://dog.ceo/api/breeds/image/random')
+    .then ((data)=> data.json())
+    .then ((data) =>{
+        setDogImage(data.message)
+        setIsLoaded(true)
+    })
+ }, [])   
+ return (
+    <div>
+       {isLoaded?
+      ( <img src={dogImage} alt="A Random Dog"></img> ,
+      <img src={dogImage} alt="A Random Dog"></img> ,
+      <img src={dogImage} alt="A Random Dog"></img> ,
+      <img src={dogImage} alt="A Random Dog"></img>  ) :
+      (<p>Loading...</p>)
+    } 
+    </div>
+ )
+}
+export default App
